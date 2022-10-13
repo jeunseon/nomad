fd7a551e682fb3d786d681a83bec261373b02502
jeunseon <euns420@naver.com>
2022-10-13 16:20:13 +0900

    painting drawings-mousemove 색상변화

--- meme_maker/js/app.js
+++ meme_maker/js/app.js
@@ -3,12 +3,27 @@ const ctx = canvas.getContext("2d"); // 일종의 붓
 canvas.width = 800;
 canvas.height = 800;
 
+const colors = [
+    "#2ecc71",
+    "#9b59b6",
+    "#c0392b",
+    "#34495e",
+    "#16a085",
+    "#d35400",
+    "#bdc3c7",
+    "#2980b9",
+    "#6ab04c",
+    "#7ed6df",
+];
+
 ctx. lineWidth = 2;
 function onClick(e){
-    e.preventDefault();
+    ctx.beginPath(); // 움직일때마다 하나의 선이 각각 변하도록
     ctx.moveTo(0,0);
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    ctx.strokeStyle = color;
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
 }
 
-canvas.addEventListener("click", onClick);
+canvas.addEventListener("mousemove", onClick);
